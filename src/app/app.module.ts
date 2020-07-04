f7512970789de8be258136cf27279524e7a19690
Ashok <ashok.kasilo@gmail.com>
2020-07-04 22:57:06 +0530

    all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MovieService } from "./services/movie.service"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMovieComponent,
+    MovieDetailsComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
